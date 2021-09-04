da73208b876ed06a1830bc11186fff06ad395b1d
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-09-05 00:19:44 +0300

    Добавляет решение без local storage

---
+++ src/Components/ContactForm/ContactForm.jsx
@@ -0,0 +1,152 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import s from './ContactForm.module.css';
+
+export default function ContactForm(options) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const formNameId = uuidv4();
+  const formNumberId = uuidv4();
+
+  const handleChangeName = event => {
+    const type = event.currentTarget.value;
+    setName(type);
+  };
+
+  const handleChangeNumber = event => {
+    const value = event.currentTarget.value;
+    setNumber(value);
+  };
+
+  const resetForm = () => {
+    setName('');
+    setNumber('');
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    options.onSubmit(name, number);
+    resetForm();
+  };
+
+  return (
+    <form className={s.form} onSubmit={handleSubmit}>
+      <label className={s.label} htmlFor={formNameId}>
+        Name{' '}
+        <input
+          className={s.input}
+          id={formNameId}
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          value={name}
+          onChange={handleChangeName}
+          placeholder={'Add name...'}
+        />
+      </label>
+
+      <label className={s.label} htmlFor={formNumberId}>
+        Number{' '}
+        <input
+          className={s.input}
+          id={formNumberId}
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+          required
+          value={number}
+          onChange={handleChangeNumber}
+          placeholder={'Add phone number...'}
+        />
+      </label>
+
+      <button className={s.button} type="submit">
+        Add contact
+      </button>
+    </form>
+  );
+}
+
+// class ContactForm extends Component {
+//   static defaultProps = {
+//     name: '',
+//     number: '',
+//   };
+
+//   state = {
+//     name: this.props.name,
+//     number: this.props.number,
+//   };
+
+//   formNameId = uuidv4();
+//   formNumberId = uuidv4();
+
+//   handleChange = event => {
+//     const { name, value } = event.currentTarget;
+//     this.setState({
+//       [name]: value,
+//     });
+//   };
+
+//   handleSubmit = event => {
+//     event.preventDefault();
+
+//     this.props.onSubmit(this.state);
+//     this.resetForm();
+//   };
+
+//   resetForm = () => {
+//     this.setState({
+//       name: '',
+//       number: '',
+//     });
+//   };
+
+//   render() {
+//     return (
+//       <form className={s.form} onSubmit={this.handleSubmit}>
+//         <label className={s.label} htmlFor={this.formNameId}>
+//           Name{' '}
+//           <input
+//             className={s.input}
+//             id={this.formNameId}
+//             type="text"
+//             name="name"
+//             pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+//             title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+//             required
+//             value={this.state.name}
+//             onChange={this.handleChange}
+//             placeholder={'Add name...'}
+//           />
+//         </label>
+
+//         <label className={s.label} htmlFor={this.formNumberId}>
+//           Number{' '}
+//           <input
+//             className={s.input}
+//             id={this.formNumberId}
+//             type="tel"
+//             name="number"
+//             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+//             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
+//             required
+//             value={this.state.number}
+//             onChange={this.handleChange}
+//             placeholder={'Add phone number...'}
+//           />
+//         </label>
+
+//         <button className={s.button} type="submit">
+//           Add contact
+//         </button>
+//       </form>
+//     );
+//   }
+// }
+
+// export default ContactForm;
--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,90 @@
 import './App.css';
+import { v4 as uuidv4 } from 'uuid';
+import Section from './Section';
 
-function App() {
-  return <div className="App">HELLO</div>;
+import React, { Component } from 'react';
+import { useState } from 'react';
+import Container from './Container';
+import ContactForm from './ContactForm';
+import ContactList from './ContactList';
+import Filter from './Filter';
+import Notification from './Notification';
+
+export default function App() {
+  const [contacts, setContacts] = useState([
+    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  ]);
+  const [filter, setFilter] = useState('');
+
+  //  componentDidUpdate(prevProps, prevState) {
+  //   const { contacts } = this.state;
+  //   if (contacts !== prevState.contacts) {
+  //     localStorage.setItem('contacts', JSON.stringify(contacts));
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   const contacts = localStorage.getItem('contacts');
+  //   const parsedContacts = JSON.parse(contacts);
+
+  //   if (parsedContacts) {
+  //     this.setState({ contacts: parsedContacts });
+  //   }
+  // }
+
+  const submitFormHandler = (name, number) => {
+    const idContact = uuidv4();
+
+    const contact = {
+      name: name,
+      number: number,
+      id: idContact,
+    };
+
+    const findCopyContact = contacts
+      .map(contact => contact.name.toLowerCase())
+      .includes(name.toLowerCase());
+
+    if (findCopyContact) {
+      alert(`${name} is already in contacts.`);
+    } else {
+      setContacts([contact, ...contacts]);
+    }
+  };
+
+  const filterChange = event => {
+    setFilter(event.currentTarget.value);
+  };
+
+  const normalizedFilter = filter.toLowerCase();
+
+  const visibleContact = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(normalizedFilter),
+  );
+
+  const deleteContact = contactId => {
+    setContacts(contacts.filter(contact => contact.id !== contactId));
+  };
+
+  return (
+    <Container>
+      <Section title="Phonebook">
+        <ContactForm onSubmit={submitFormHandler} />
+      </Section>
+      <Section title="Contacts">
+        <Filter value={filter} onChange={filterChange}></Filter>
+        {contacts.length > 0 ? (
+          <ContactList
+            contacts={visibleContact}
+            onDeleteContact={deleteContact}
+          ></ContactList>
+        ) : (
+          <Notification message="No contacts"></Notification>
+        )}
+      </Section>
+    </Container>
+  );
 }
-
-export default App;

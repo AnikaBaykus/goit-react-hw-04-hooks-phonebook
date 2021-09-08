4423d4b5a67a8f2d0d9150d5d6665c58858f04dc
Anna_Matveeva <75207035+AnikaBaykus@users.noreply.github.com>
2021-09-08 17:19:05 +0300

    Исправляет ошибки ДЗ

--- src/components/App.js
+++ src/components/App.js
@@ -9,12 +9,7 @@ import Filter from './Filter';
 import Notification from './Notification';
 
 export default function App() {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
 
   useEffect(() => {
@@ -71,12 +66,14 @@ export default function App() {
         <ContactForm onSubmit={submitFormHandler} />
       </Section>
       <Section title="Contacts">
-        <Filter value={filter} onChange={filterChange}></Filter>
         {contacts.length > 0 ? (
-          <ContactList
-            contacts={visibleContact}
-            onDeleteContact={deleteContact}
-          ></ContactList>
+          <>
+            <Filter value={filter} onChange={filterChange}></Filter>
+            <ContactList
+              contacts={visibleContact}
+              onDeleteContact={deleteContact}
+            ></ContactList>
+          </>
         ) : (
           <Notification message="No contacts"></Notification>
         )}
